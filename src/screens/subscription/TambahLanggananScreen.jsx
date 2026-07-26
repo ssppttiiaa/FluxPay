2ec902af975cdaffdc66c14d0f5b7757333879ad
Robby Dwi Pamungkas <robbydwi2006@gmail.com>
2026-07-26 19:16:00 +0700

    push ulang

--- src/screens/subscription/TambahLanggananScreen.jsx
+++ src/screens/subscription/TambahLanggananScreen.jsx
@@ -20,6 +20,7 @@ import { colors } from "../../constants/colors";
 
 import SubscriptionApi from "../../api/SubscriptionApi";
 import SubscriptionModel from "../../models/SubscriptionModel";
+import LocalNotificationService from "../../services/LocalNotificationService";
 
 export default function TambahLanggananScreen({ navigation }) {
   const [formData, setFormData] = useState({
@@ -33,18 +34,7 @@ export default function TambahLanggananScreen({ navigation }) {
   const categories = ["HIBURAN", "TOOLS", "KERJA", "PRODUKTIVITAS"];
 
   const handleSave = async () => {
-    // if (
-    //   !formData.name ||
-    //   !formData.price ||
-    //   !formData.dueDate ||
-    //   !formData.category
-    // ) {
-    //   Alert.alert(
-    //     "Peringatan",
-    //     "Harap isi semua field yang wajib!"
-    //   );
-    //   return;
-    // }
+
 
     console.log("===== HANDLE SAVE DIJALANKAN =====");
 
@@ -87,7 +77,14 @@ export default function TambahLanggananScreen({ navigation }) {
       });
 
       console.log("Sebelum create");
-      await SubscriptionApi.create(subscription);
+      const insertedId =
+        await SubscriptionApi.create(subscription);
+
+      subscription.id = insertedId;
+
+      await LocalNotificationService.scheduleSubscriptionReminders(
+        subscription
+      );
       console.log("Sesudah create");
 
       setFormData({
@@ -239,8 +236,7 @@ export default function TambahLanggananScreen({ navigation }) {
               </View>
 
               <Text style={styles.reminderText}>
-                FluxPay akan mengingatkan Anda 2 hari sebelum tanggal
-                pembayaran.
+                FluxPay akan mengingatkan Anda 7, 3, dan 1 hari sebelum tanggal pembayaran.
               </Text>
             </View>
           </View>
